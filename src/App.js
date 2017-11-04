1b030154dd3d0e2e28931e53b21030c5adf0a1a1
joseteodoro <jteodoro.dev@gmail.com>
2017-11-03 23:43:31 -0200

    simplifing the component set

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
-import Categories from './components/list-categories'
 import Posts from './components/posts'
+import Post from './components/post'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Route exact path='/' render={() => (
-          <Categories />
-        )} />
-        <Route path='/category/:category_name' render={({ history }) => (
-          <Posts />
-        )} />
+        <Switch>
+          <Route exact path='/' component={Posts} />
+          <Route path='/category/:category_id' component={Posts} />
+          <Route path='/post/:post_id' component={Post} />
+          <Route path='/edit_post/:post_id' component={Post} />
+        </Switch>
       </div>
     );
   }
